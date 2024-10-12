53efb0595becea66dbbe86cf9689e6ff815a82bb
devsrakib <devs.rakibulislam@gmail.com>
2024-10-12 11:30:16 +0600

    complete activity indicator

--- src/components/ActivityIndicator.tsx
+++ src/components/ActivityIndicator.tsx
@@ -1,13 +1,76 @@
 /* eslint-disable import/order */
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import {
+  BlurMask,
+  Canvas,
+  Circle,
+  Path,
+  rotate,
+  Skia,
+  SweepGradient,
+  vec,
+} from '@shopify/react-native-skia';
+import Animated, {
+  Easing,
+  interpolate,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
 
+const CanvasSize = 100;
+const CircleSize = 64;
+const strokeWidth = 10;
+const CircleRadius = (CircleSize - strokeWidth) / 2;
 const ActivityIndicator = () => {
+  const progress = useSharedValue(0);
+
+  const circlePath = useMemo(() => {
+    const skPath = Skia.Path.Make();
+    skPath.addCircle(CanvasSize / 2, CanvasSize / 2, CircleRadius);
+    return skPath;
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(
+      withTiming(1, { duration: 1000, easing: Easing.linear }),
+      -1,
+      false
+    );
+  }, [progress]);
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${progress.value * 2 * Math.PI}rad` }],
+    };
+  }, []);
+
+  const startAnimated = useDerivedValue(() => {
+    return interpolate(progress.value, [0, 0.5, 1], [0.3, 0.6, 0.3]);
+  }, []);
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <Text>ActivityIndicator</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <Animated.View style={rStyle}>
+        <Canvas style={{ width: CanvasSize, height: CanvasSize }}>
+          <Path
+            path={circlePath}
+            color="white"
+            style={'stroke'}
+            strokeWidth={strokeWidth}
+            start={startAnimated}
+            end={1}
+            strokeCap={'round'}>
+            <SweepGradient
+              c={vec(CanvasSize / 2, CanvasSize / 2)}
+              colors={['cyan', 'magenta', 'yellow', 'cyan']}
+            />
+            <BlurMask blur={10} style={'solid'} />
+          </Path>
+        </Canvas>
+      </Animated.View>
     </SafeAreaView>
   );
 };
@@ -19,5 +82,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black',
   },
 });
